f413e67839ff6904282ee7e442906dd9a48a9ac6
Samuele Cesaroni <samuele.cesaroni@gmail.com>
2023-11-21 12:52:26 +0100

    quasi funzionante

--- Ipotesi/MainWindow.xaml.cs
+++ Ipotesi/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +23,12 @@ namespace Ipotesi
     {
         private ObservableCollection<Sintomi> _listaSintomi;
         private ObservableCollection<Sintomi> _filteredSintomi;
+        private IpotesiEntities _dbContext;
 
         public MainWindow()
         {
             InitializeComponent();
-
+            _dbContext = new IpotesiEntities();
             _listaSintomi = new ObservableCollection<Sintomi>();
             _filteredSintomi = new ObservableCollection<Sintomi>();
 
@@ -41,19 +43,17 @@ namespace Ipotesi
         {
             try
             {
-                using (var dbContext = new IpotesiEntities())
+                // Usa il dbContext inizializzato all'inizio
+                var sintomi = await _dbContext.Sintomi.ToListAsync();
+
+                _listaSintomi.Clear();
+                foreach (var sintomo in sintomi)
                 {
-                    var sintomi = await Task.Run(() => dbContext.Sintomi.ToList());
-
-                    _listaSintomi.Clear();
-                    foreach (var sintomo in sintomi)
-                    {
-                        _listaSintomi.Add(sintomo);
-                    }
-
-                    // Chiamata per filtrare dopo l'aggiornamento della lista
-                    FilterSintomi();
+                    _listaSintomi.Add(sintomo);
                 }
+
+                // Chiamata per filtrare dopo l'aggiornamento della lista
+                FilterSintomi();
             }
             catch (Exception ex)
             {
@@ -61,6 +61,85 @@ namespace Ipotesi
             }
         }
 
+        private void RicercaMalattia_Click(object sender, RoutedEventArgs e)
+        {
+            var sintomiSelezionati = lstSintomi.SelectedItems.Cast<Sintomi>().ToList();
+
+            Console.WriteLine("Sintomi selezionati:");
+            foreach (var sintomo in sintomiSelezionati)
+            {
+                Console.WriteLine($"Sintomo: {sintomo.Sintomo}, Codice: {sintomo.Numeri}");
+            }
+
+            // Estrai i codici dai sintomi selezionati
+            List<string> codiciSintomi = sintomiSelezionati
+                .Select(s => s.Numeri)
+                .SelectMany(c => c.Split(' ')) // Utilizza SelectMany per "appiattire" gli array di stringhe
+                .Where(c => !string.IsNullOrWhiteSpace(c)) // Filtra gli spazi vuoti
+                .ToList();
+
+            Console.WriteLine("Codici sintomi estratti:");
+            foreach (var codiceSintomo in codiciSintomi)
+            {
+                Console.WriteLine($"Codice sintomo: {codiceSintomo}");
+            }
+
+            // Dictionary per tenere traccia delle frequenze dei codici delle malattie
+            Dictionary<string, int> frequenzeMalattie = new Dictionary<string, int>();
+
+            // Ora hai una lista di codici singoli da confrontare con i codici delle malattie
+            // Esegui la tua logica di confronto qui con i codici delle malattie
+            foreach (var codiceSintomo in codiciSintomi)
+            {
+                // Controlla con i codici delle malattie
+                // Ad esempio:
+                var malattieCorrispondenti = _dbContext.Malattie
+                .AsEnumerable()
+                .Where(m => m.Codice != null && m.Codice.Split(' ').Any(num => num == codiceSintomo))
+                .Select(m => m.Malattia)
+                .ToList();
+
+                foreach (var malattia in malattieCorrispondenti)
+                {
+                    // Aggiorna la frequenza delle malattie nel dizionario
+                    if (frequenzeMalattie.ContainsKey(malattia))
+                    {
+                        frequenzeMalattie[malattia]++;
+                    }
+                    else
+                    {
+                        frequenzeMalattie.Add(malattia, 1);
+                    }
+                }
+            }
+          
+
+            var malattiaPiùProbabile = frequenzeMalattie.OrderByDescending(kv => kv.Value).FirstOrDefault();
+
+            if (malattiaPiùProbabile.Value > 0)
+            {
+                Console.WriteLine($"Malattia più probabile: {malattiaPiùProbabile.Key} (Frequenza: {malattiaPiùProbabile.Value})");
+
+                // Aggiorna la TextBox per mostrare la malattia più probabile
+                txtMalattieTrovate.Text = malattiaPiùProbabile.Key;
+            }
+            else
+            {
+                Console.WriteLine("Nessuna malattia trovata.");
+                // Se vuoi gestire il caso in cui non è stata trovata alcuna malattia probabile, puoi
+                // inserire qui la logica appropriata.
+            }
+
+            // Pulisci la selezione dopo la ricerca
+            lstSintomi.SelectedItems.Clear();
+            FilterSintomi();
+        }
+
+
+        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterSintomi();
+        }
 
         private void FilterSintomi()
         {
@@ -77,12 +156,6 @@ namespace Ipotesi
                     }
                 }
             });
-        }
-
-
-        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            FilterSintomi();
         }
     }
 }
